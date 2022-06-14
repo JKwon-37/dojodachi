e62844b942fd997b1b0bbbc4cecb34b11baa7cc6
Jonathan Kwon <jonathankwon@yahoo.com>
2022-06-14 02:13:17 -0700

    updated Play, Work, and Sleep methods, and added clear session in Index.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -49,12 +49,22 @@ public class HomeController : Controller
         string? updateStatus = HttpContext.Session.GetString("Status");
         if (numOfMeals > 0)
         {
-            numOfMeals--;
-            fullnessLvl += rand.Next(5,11);
-            updateStatus = "*Munch munch munch* Yummy!";
-            HttpContext.Session.SetInt32("Meals", (int)numOfMeals);
-            HttpContext.Session.SetInt32("Fullness", (int)fullnessLvl);
-            HttpContext.Session.SetString("Status", updateStatus);
+            int? chance = rand.Next(0,4);
+            if(chance == 0)
+            {
+                numOfMeals--;
+                updateStatus = "I wasn't hungry though...";
+                HttpContext.Session.SetInt32("Meals", (int)numOfMeals);
+                HttpContext.Session.SetString("Status", updateStatus);
+            } else
+            {
+                numOfMeals--;
+                fullnessLvl += rand.Next(5,11);
+                updateStatus = "*Munch munch munch* Yummy!";
+                HttpContext.Session.SetInt32("Meals", (int)numOfMeals);
+                HttpContext.Session.SetInt32("Fullness", (int)fullnessLvl);
+                HttpContext.Session.SetString("Status", updateStatus);
+            }
         } else
         {
             updateStatus = "Wait, there's no meal...";
@@ -62,24 +72,92 @@ public class HomeController : Controller
         }
         return RedirectToAction("Index");
     }
-    
-    public string Play()
+
+    [HttpPost("play")]
+    public IActionResult Play()
     {
-        return "Play meh";
-    }
-    public string Work()
-    {
-        return "Work meh";
-    }
-    public string Sleep()
-    {
-        return "Sleep meh";
+        Random rand = new Random();
+        int? energyLvl = HttpContext.Session.GetInt32("Energy");
+        int? happinessLvl = HttpContext.Session.GetInt32("Happiness");
+        string? updateStatus = HttpContext.Session.GetString("Status");
+        if (energyLvl > 0)
+        {
+            int chance = rand.Next(0,4);
+            if (chance == 0){
+                energyLvl -= 5;
+                updateStatus = "I didn't really feel like playing though...";
+                HttpContext.Session.SetInt32("Energy", (int)energyLvl);
+                HttpContext.Session.SetString("Status", updateStatus);
+            } else 
+            {
+                energyLvl -= 5;
+                happinessLvl += rand.Next(5,11);
+                updateStatus = "So fun!  Yay!";
+                HttpContext.Session.SetInt32("Energy", (int)energyLvl);
+                HttpContext.Session.SetInt32("Happiness", (int)happinessLvl);
+                HttpContext.Session.SetString("Status", updateStatus);
+            }
+        } else
+        {
+            updateStatus = "Chopper has fainted from exhaustion...";
+            HttpContext.Session.SetString("Status", updateStatus);
+        }
+        return RedirectToAction("Index");
     }
 
-    [HttpGet("clear")]
-    public IActionResult ClearSession()
+    [HttpPost("work")]
+    public IActionResult Work()
     {
-        HttpContext.Session.Remove("Reset");
+        Random rand = new Random();
+        int? energyLvl = HttpContext.Session.GetInt32("Energy");
+        int? numOfMeals = HttpContext.Session.GetInt32("Meals");
+        string? updateStatus = HttpContext.Session.GetString("Status");
+
+        if (energyLvl > 0)
+        {
+            energyLvl -= 5;
+            numOfMeals += rand.Next(1,4);
+            updateStatus = "Right! Let's do our best!";
+            HttpContext.Session.SetInt32("Energy", (int)energyLvl);
+            HttpContext.Session.SetInt32("Meals", (int)numOfMeals);
+            HttpContext.Session.SetString("Status", updateStatus);
+        } else
+        {
+            updateStatus = "Chopper has fainted from exhaustion...";
+            HttpContext.Session.SetString("Status", updateStatus);
+        } 
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost("sleep")]
+    public IActionResult Sleep()
+    {
+        int? energyLvl = HttpContext.Session.GetInt32("Energy");
+        int? fullnessLvl = HttpContext.Session.GetInt32("Fullness");
+        int? happinessLvl = HttpContext.Session.GetInt32("Happiness");
+        string? updateStatus = HttpContext.Session.GetString("Status");
+
+        if (energyLvl > 0)
+        {
+            energyLvl += 15;
+            fullnessLvl -= 5;
+            happinessLvl -=5;
+            updateStatus = "Zzzzzzzz *snores*";
+            HttpContext.Session.SetInt32("Energy", (int)energyLvl);
+            HttpContext.Session.SetInt32("Fullness", (int)fullnessLvl);
+            HttpContext.Session.SetInt32("Happiness", (int)happinessLvl);
+        } else
+        {
+            updateStatus = "Chopper has fainted from exhaustion...";
+            HttpContext.Session.SetString("Status", updateStatus);
+        }
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost("clear")]
+    public IActionResult Restart()
+    {
+        HttpContext.Session.Clear();
         return RedirectToAction("Index");
     }
 
